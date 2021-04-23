cb20407789b7d8cf219f6c4eebe87628a39e3ab8
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-04-24 00:54:31 +0530

    refactore: fix indentation

--- src/main.js
+++ src/main.js
@@ -1,77 +1,73 @@
 class Registration {
-
-    firstName = (name) => {
-        let regex = new RegExp("^[A-Z]{1}[a-z]{2,}$");
-        if(name.match(regex)) {
-            console.log("Entered Name Valid");
-            return true;
-        }
-        else{
-            console.log("Entered Name InValid");
-            return false;
-        } 
+  firstName = (name) => {
+    let regex = new RegExp("^[A-Z]{1}[a-z]{2,}$");
+    if (name.match(regex)) {
+      console.log("Entered Name Valid");
+      return true;
+    } else {
+      console.log("Entered Name InValid");
+      return false;
     }
+  };
 
-    lastName = (lastName) => {
-        let regex = new RegExp("^[A-Z]{1}[a-z]{2,}$");
-        if(lastName.match(regex)){
-            console.log("Enterd Last Name is Valid");
-            return true;
-        }
-        else {
-            console.log("Enterd Last Name is InValid");
-            return false;
-        }  
+  lastName = (lastName) => {
+    let regex = new RegExp("^[A-Z]{1}[a-z]{2,}$");
+    if (lastName.match(regex)) {
+      console.log("Enterd Last Name is Valid");
+      return true;
+    } else {
+      console.log("Enterd Last Name is InValid");
+      return false;
     }
+  };
 
-    email = (email) => {
-        let regex = new RegExp("abc[a-zA-Z0-9.]*@bl[.]co[.][a-z]{2,3}")
-        if(email.match(regex)){
-            console.log("Correct Email");
-            return true;
-        } 
-        else {
-            console.log("InCorrect Email");
-            return false;
-        }  
+  email = (email) => {
+    let regex = new RegExp("abc[a-zA-Z0-9.]*@bl[.]co[.][a-z]{2,3}");
+    if (email.match(regex)) {
+      console.log("Correct Email");
+      return true;
+    } else {
+      console.log("InCorrect Email");
+      return false;
     }
+  };
 
-    phone = (phone) => {
-        let regex = new RegExp("^[0-9]{2}[: :][0-9]{10}$")
-        if(phone.match(regex)){
-            console.log("Correct Phone Number");
-            return true;
-        } 
-        else {
-            console.log("InCorrect Phone Number");
-            return false;
-        }  
+  phone = (phone) => {
+    let regex = new RegExp("^[0-9]{2}[: :][0-9]{10}$");
+    if (phone.match(regex)) {
+      console.log("Correct Phone Number");
+      return true;
+    } else {
+      console.log("InCorrect Phone Number");
+      return false;
     }
+  };
 
-    password = (password) => {
-        let regex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()]{1}).{8,}$")
-        if(password.match(regex)){
-            console.log("Valid Password");
-            return true;
-        } 
-        else {
-            console.log("Invalid Password");
-            return false;
-        }  
+  password = (password) => {
+    let regex = new RegExp(
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()]{1}).{8,}$"
+    );
+    if (password.match(regex)) {
+      console.log("Valid Password");
+      return true;
+    } else {
+      console.log("Invalid Password");
+      return false;
     }
+  };
 
-    emailSample = (email) => {
-        let regex = new RegExp("^abc[a-zA-Z0-9.+-]*@[a-z0-9]*[.][a-z]{2,5}[.,a-z]*$")
-        if(email.match(regex)){
-            console.log("Correct Email");
-            return true;
-        } 
-        else {
-            console.log("InCorrect Email");
-            return false;
-        }  
+  emailSample = (email) => {
+    let regex = new RegExp(
+      "^abc[a-zA-Z0-9.+-]*@[a-z0-9]*[.][a-z]{2,5}[.,a-z]*$"
+    );
+    if (email.match(regex)) {
+      console.log("Correct Email");
+      return true;
+    } else {
+      console.log("InCorrect Email");
+      return false;
     }
-    
+  };
 }
 
 module.exports = new Registration();
--- test/mainTest.js
+++ test/mainTest.js
@@ -1,154 +1,154 @@
-const assert = require('chai').assert
-const mainObj = require('../src/main');
+const assert = require("chai").assert;
+const mainObj = require("../src/main");
 
-describe("Valid First Name", ()=> {
-    it('should be true when name is correct', ()=> {
-       var result = mainObj.firstName("Deep")
-       assert.equal(result, true)
-    });
+describe("Valid First Name", () => {
+  it("should be true when name is correct", () => {
+    var result = mainObj.firstName("Deep");
+    assert.equal(result, true);
+  });
 
-    it('should be fail when name is incorrect', ()=> {
-        var result = mainObj.firstName("deep")
-        assert.equal(result, false)
-     });
+  it("should be fail when name is incorrect", () => {
+    var result = mainObj.firstName("deep");
+    assert.equal(result, false);
+  });
 
-     it('should be fail when name is less than 3 character', ()=> {
-        var result = mainObj.firstName("De")
-        assert.equal(result, false)
-     });
+  it("should be fail when name is less than 3 character", () => {
+    var result = mainObj.firstName("De");
+    assert.equal(result, false);
+  });
 });
 
-describe("Valid Last Name", ()=> {
-    it('should be true when name is correct', ()=> {
-       let result = mainObj.lastName("Deep")
-       assert.equal(result, true)
-    });
+describe("Valid Last Name", () => {
+  it("should be true when name is correct", () => {
+    let result = mainObj.lastName("Deep");
+    assert.equal(result, true);
+  });
 
-    it('should be fail when name is incorrect', ()=> {
-        var result = mainObj.lastName("deep")
-        assert.equal(result, false)
-     });
+  it("should be fail when name is incorrect", () => {
+    var result = mainObj.lastName("deep");
+    assert.equal(result, false);
+  });
 
-     it('should be fail when name is less than 3 character', ()=> {
-        var result = mainObj.lastName("De")
-        assert.equal(result, false)
-     });
+  it("should be fail when name is less than 3 character", () => {
+    var result = mainObj.lastName("De");
+    assert.equal(result, false);
+  });
 });
 
-describe("Valid Email", ()=> {
-    it('should be true when eamil is correct', ()=> {
-       let result = mainObj.email("abc.xyz@bl.co.in")
-       assert.equal(result, true)
-    });
+describe("Valid Email", () => {
+  it("should be true when eamil is correct", () => {
+    let result = mainObj.email("abc.xyz@bl.co.in");
+    assert.equal(result, true);
+  });
 
-    it('should be fail when email is incorrect', ()=> {
-        var result = mainObj.email("abc.xyz@bldfkjs.co.in")
-        assert.equal(result, false)
-     });
+  it("should be fail when email is incorrect", () => {
+    var result = mainObj.email("abc.xyz@bldfkjs.co.in");
+    assert.equal(result, false);
+  });
 
-     it('should be fail when email is incorrect', ()=> {
-        var result = mainObj.email("xyz@bldfkjs.co.in")
-        assert.equal(result, false)
-     });
+  it("should be fail when email is incorrect", () => {
+    var result = mainObj.email("xyz@bldfkjs.co.in");
+    assert.equal(result, false);
+  });
 });
 
-describe("Valid Phone", ()=> {
-    it('should be true when phone number is correct', ()=> {
-       let result = mainObj.phone("91 3424323244")
-       assert.equal(result, true)
-    });
+describe("Valid Phone", () => {
+  it("should be true when phone number is correct", () => {
+    let result = mainObj.phone("91 3424323244");
+    assert.equal(result, true);
+  });
 
-    it('should be fail when phone number is more then 10 digits', ()=> {
-        var result = mainObj.phone("13 43232432448")
-        assert.equal(result, false)
-     });
+  it("should be fail when phone number is more then 10 digits", () => {
+    var result = mainObj.phone("13 43232432448");
+    assert.equal(result, false);
+  });
 
-     it('should be fail when phone is less then 10 digits', ()=> {
-        var result = mainObj.phone("23 21312312")
-        assert.equal(result, false)
-     });
+  it("should be fail when phone is less then 10 digits", () => {
+    var result = mainObj.phone("23 21312312");
+    assert.equal(result, false);
+  });
 });
 
-describe("Valid Password", ()=> {
-    it('should be true when password has minimum 8 character', ()=> {
-       let result = mainObj.password("dsDd@3442")
-       assert.equal(result, true)
-    });
+describe("Valid Password", () => {
+  it("should be true when password has minimum 8 character", () => {
+    let result = mainObj.password("dsDd@3442");
+    assert.equal(result, true);
+  });
 
-    it('should be fail when password has less then 8 character', ()=> {
-        let result = mainObj.password("dD@32")
-        assert.equal(result, false)
-     });
+  it("should be fail when password has less then 8 character", () => {
+    let result = mainObj.password("dD@32");
+    assert.equal(result, false);
+  });
 
-     it('should be true when password has atleat one upper case', ()=> {
-        let result = mainObj.password("dsDd@3442")
-        assert.equal(result, true)
-     });
- 
-     it('should be fail when password has no upper cases', ()=> {
-         let result = mainObj.password("d@3sdff2")
-         assert.equal(result, false)
-      });
+  it("should be true when password has atleat one upper case", () => {
+    let result = mainObj.password("dsDd@3442");
+    assert.equal(result, true);
+  });
 
-      it('should be true when password has atleat one numeric', ()=> {
-        let result = mainObj.password("dsDd@3442")
-        assert.equal(result, true)
-     });
- 
-     it('should be fail when password has no numeric', ()=> {
-         let result = mainObj.password("d@DFdfffd")
-         assert.equal(result, false)
-      });
+  it("should be fail when password has no upper cases", () => {
+    let result = mainObj.password("d@3sdff2");
+    assert.equal(result, false);
+  });
 
-      it('should be true when password has exactly 1 special char', ()=> {
-        let result = mainObj.password("dsDd@3442")
-        assert.equal(result, true)
-     });
- 
-     it('should be fail when password has no special Char', ()=> {
-         let result = mainObj.password("dDFdfffd12")
-         assert.equal(result, false)
-      });
+  it("should be true when password has atleat one numeric", () => {
+    let result = mainObj.password("dsDd@3442");
+    assert.equal(result, true);
+  });
+
+  it("should be fail when password has no numeric", () => {
+    let result = mainObj.password("d@DFdfffd");
+    assert.equal(result, false);
+  });
+
+  it("should be true when password has exactly 1 special char", () => {
+    let result = mainObj.password("dsDd@3442");
+    assert.equal(result, true);
+  });
+
+  it("should be fail when password has no special Char", () => {
+    let result = mainObj.password("dDFdfffd12");
+    assert.equal(result, false);
+  });
 });
 
-describe("Valid Email", ()=> {
-    it('should be true when eamil is correct', ()=> {
-       let result = mainObj.emailSample("abc-100@yahoo.com")
-       assert.equal(result, true)
-    });
+describe("Valid Email", () => {
+  it("should be true when eamil is correct", () => {
+    let result = mainObj.emailSample("abc-100@yahoo.com");
+    assert.equal(result, true);
+  });
 
-    it('should be fail when email is incorrect', ()=> {
-        var result = mainObj.emailSample("abc.100@yahoo.com")
-        assert.equal(result, true)
-     });
+  it("should be fail when email is incorrect", () => {
+    var result = mainObj.emailSample("abc.100@yahoo.com");
+    assert.equal(result, true);
+  });
 
-     it('should be fail when email is incorrect', ()=> {
-        var result = mainObj.emailSample("abc111@abc.com")
-        assert.equal(result, true)
-     });
+  it("should be fail when email is incorrect", () => {
+    var result = mainObj.emailSample("abc111@abc.com");
+    assert.equal(result, true);
+  });
 
-     it('should be fail when email is incorrect', ()=> {
-        var result = mainObj.emailSample("abc-100@abc.net")
-        assert.equal(result, true)
-     });
+  it("should be fail when email is incorrect", () => {
+    var result = mainObj.emailSample("abc-100@abc.net");
+    assert.equal(result, true);
+  });
 
-     it('should be fail when email is incorrect', ()=> {
-        var result = mainObj.emailSample("abc.100@abc.com.au")
-        assert.equal(result, true)
-     });
+  it("should be fail when email is incorrect", () => {
+    var result = mainObj.emailSample("abc.100@abc.com.au");
+    assert.equal(result, true);
+  });
 
-     it('should be fail when email is incorrect', ()=> {
-        var result = mainObj.emailSample("abc@1.com")
-        assert.equal(result, true)
-     });
+  it("should be fail when email is incorrect", () => {
+    var result = mainObj.emailSample("abc@1.com");
+    assert.equal(result, true);
+  });
 
-     it('should be fail when email is incorrect', ()=> {
-        var result = mainObj.emailSample("abc@gmail.com.com")
-        assert.equal(result, true)
-     });
+  it("should be fail when email is incorrect", () => {
+    var result = mainObj.emailSample("abc@gmail.com.com");
+    assert.equal(result, true);
+  });
 
-     it('should be fail when email is incorrect', ()=> {
-        var result = mainObj.emailSample("abc+100@gamil.com")
-        assert.equal(result, true)
-     });
+  it("should be fail when email is incorrect", () => {
+    var result = mainObj.emailSample("abc+100@gamil.com");
+    assert.equal(result, true);
+  });
 });
